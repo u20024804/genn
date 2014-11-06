e4a1abd5f1dcca989f0387573a91ef0f4277901b
Alex Cope <a.cope@shef.ac.uk>
2014-11-06 15:18:03 +0000

    Some improvements...

---
+++ lib/include/extra_weightupdates.h
@@ -0,0 +1,58 @@
+
+			
+  // Add new weightupdate type - static weight: 
+  wu.varNames.clear();
+  wu.varTypes.clear();
+  
+  wu.pNames.clear();
+  
+  wu.pNames.push_back(tS("g_WU"));
+  //wu.dpNames.clear();
+
+  wu.simCode = tS(" \
+      { \n \
+     } \n \
+  	 	linSyn = $(g_WU); \n \
+");
+
+  weightUpdateModels.push_back(wu);
+
+	
+			
+  // Add new weightupdate type - static weight: 
+  wu.varNames.clear();
+  wu.varTypes.clear();
+  
+  wu.pNames.clear();
+  
+  wu.pNames.push_back(tS("g_WU"));
+  //wu.dpNames.clear();
+
+  wu.simCode = tS(" \
+      { \n \
+     } \n \
+  	 	linSyn = $(g_WU); \n \
+");
+
+  weightUpdateModels.push_back(wu);
+
+	
+			
+  // Add new weightupdate type - static weight: 
+  wu.varNames.clear();
+  wu.varTypes.clear();
+  
+  wu.pNames.clear();
+  
+  wu.pNames.push_back(tS("g_WU"));
+  //wu.dpNames.clear();
+
+  wu.simCode = tS(" \
+      { \n \
+     } \n \
+  	 	linSyn = $(g_WU); \n \
+");
+
+  weightUpdateModels.push_back(wu);
+
+	
